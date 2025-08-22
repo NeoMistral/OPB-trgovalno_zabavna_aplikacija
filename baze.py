517d7c23bcdeb189436ff6fba874e3f02ff3ef91
Benjamin Levicar <bl8491@student.uni-lj.si>
2025-08-22 11:49:16 +0200

    changed baze

--- baze.py
+++ baze.py
@@ -24,6 +24,10 @@ try:
     # drop_uporabniki = "DROP TABLE IF EXISTS Uporabniki CASCADE;"
     # drop_denarnica = "DROP TABLE IF EXISTS Denarnica CASCADE;"
     # drop_vrednostni_papirji = "DROP TABLE IF EXISTS vrednostni_papirji CASCADE;"
+    # drop_portfelji = "DROP TABLE IF EXISTS Portfelji CASCADE;"
+    drop_transakcije = "DROP TABLE IF EXISTS Transakcije CASCADE;"
+    cur.execute(drop_transakcije)
+    # cur.execute(drop_portfelji)
     # cur.execute(drop_denarnica)
     # cur.execute(drop_uporabniki)
     # cur.execute(drop_vrednostni_papirji)
@@ -34,19 +38,20 @@ try:
         uporabnik_id SERIAL PRIMARY KEY,
         uporabnisko_ime VARCHAR(50) NOT NULL UNIQUE,
         geslo VARCHAR(100) NOT NULL,
-        denarnica_id INTEGER 
+        stanje DECIMAL(10, 2) NOT NULL,
+        vrednostni_portfeljev INTEGER NOT NULL,
     );
     """
     
-    create_denarnica = """
-    CREATE TABLE IF NOT EXISTS Denarnica (
-        denarnica_id SERIAL PRIMARY KEY,
-        uporabnik_id INTEGER NOT NULL,
-        vrednostni_portfeljev INTEGER NOT NULL,
-        stanje DECIMAL(10, 2) NOT NULL,
-        FOREIGN KEY (uporabnik_id) REFERENCES Uporabniki(uporabnik_id)
-    );
-    """
+    # create_denarnica = """
+    # CREATE TABLE IF NOT EXISTS Denarnica (
+    #     denarnica_id SERIAL PRIMARY KEY,
+    #     uporabnik_id INTEGER NOT NULL,
+    #     vrednostni_portfeljev INTEGER NOT NULL,
+    #     stanje DECIMAL(10, 2) NOT NULL,
+    #     FOREIGN KEY (uporabnik_id) REFERENCES Uporabniki(uporabnik_id)
+    # );
+    # """
     
     # create_vrednostni_papirji = """
     # CREATE TABLE IF NOT EXISTS Vrednostni_papirji (
@@ -59,24 +64,22 @@ try:
     create_transakcije = """
     CREATE TABLE IF NOT EXISTS Transakcije (
         transakcija_id SERIAL PRIMARY KEY,
-        vrednostni_papir_id INTEGER NOT NULL,
+        simbol varchar(10) NOT NULL,
         uporabnik_id INTEGER NOT NULL,
         kolicina INTEGER NOT NULL,
         vrednost DECIMAL(10, 2) NOT NULL,
         datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (vrednostni_papir_id) REFERENCES delnice(vrednostni_papir_id),
         FOREIGN KEY (uporabnik_id) REFERENCES Uporabniki(uporabnik_id)
     );
     """
     
     create_portfelji = """
     CREATE TABLE IF NOT EXISTS Portfelji (
+        portfelj_id SERIAL PRIMARY KEY,
         uporabnik_id INTEGER NOT NULL,
-        vrednostni_papir_id INTEGER NOT NULL,
+        simbol VARCHAR(10) NOT NULL,
         kolicina INTEGER NOT NULL,
         vrednost DECIMAL(10, 2) NOT NULL,
-        PRIMARY KEY (uporabnik_id, vrednostni_papir_id),
-        FOREIGN KEY (vrednostni_papir_id) REFERENCES delnice(vrednostni_papir_id),
         FOREIGN KEY (uporabnik_id) REFERENCES Uporabniki(uporabnik_id)
     );
     """
@@ -93,12 +96,12 @@ try:
         FOREIGN KEY (uporabnik_id) REFERENCES Uporabniki(uporabnik_id)
     );
     """
-    cur.execute(create_uporabniki)
-    cur.execute(create_denarnica)
-    #cur.execute(create_vrednostni_papirji)
+    # cur.execute(create_uporabniki)
+    # cur.execute(create_denarnica)
+    # cur.execute(create_vrednostni_papirji)
     cur.execute(create_transakcije)
-    cur.execute(create_portfelji)
-    cur.execute(create_poker)
+    # cur.execute(create_portfelji)
+    # cur.execute(create_poker)
     
     conn.commit()
 
